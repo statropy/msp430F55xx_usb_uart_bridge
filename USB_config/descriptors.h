0a9b526dc111ab39fa411d19c24987d8a01b921f
Erik Larson <erik@statropy.com>
2020-10-30 11:56:05 -0400

    Update VID/PID and USB description strings
    Signed-off-by: Erik Larson <erik@statropy.com>

--- USB_config/descriptors.h
+++ USB_config/descriptors.h
@@ -64,8 +64,8 @@ extern "C"
 
 // Configuration Constants that can change
 // #define that relates to Device Descriptor
-#define USB_VID               0x2fe3    // Vendor ID (VID)
-#define USB_PID               0x000d    // Product ID (PID)
+#define USB_VID               0x2047    // Vendor ID (VID)
+#define USB_PID               0x0AA5    // Product ID (PID)
 
 /*----------------------------------------------------------------------------+
 | Firmware Version                                                            |
